cacb1b17d43ce76d4b3677cb0a37a62bf3227cbd
fmglimlengco <fmglimlengco@gmail.com>
2025-01-30 14:08:12 -0500

    update 2

--- a2.js
+++ a2.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Frances Marie Limlengco Student ID: 180927238 Date: 01/30/2025
+*
+********************************************************************************/ 
+
+
 const collegeData = require('./modules/collegeData');
 
 collegeData.initialize().then(() => {
